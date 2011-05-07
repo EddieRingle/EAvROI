8b8ecfa2abb5a12e8954b5a4acc5c62f1a378ec3
Eddie Ringle <eddie@eringle.net>
2011-05-07 18:29:53 -0400

    Add better color coding; Users that give high dividends should get a bump
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- eavroi.js
+++ eavroi.js
@@ -1,20 +1,39 @@
 var currency = $('#eavtitle div.price span.currency');
 var price = $('a', currency).html().trim();
 var dividend;
+var divamt;
 try {
   dividend = $('#nmp-stats div:first > span:last > span:first > a').html().trim();
+  divamt = $('#nmp-stats div:first > span:first > span:first > a').html().trim();
 } catch(e) {
   dividend = $('#nmp-stats div:nth-child(4) > span:last > span:first > a').html().trim();
+  divamt = $('#nmp-stats div:nth-child(4) > span:first > span:first > a').html().trim();
 }
+
+divamt = divamt.replace(/\,/g, '');
+
 var roi = dividend / price;
 var roip = roi * 100;
 
+if (divamt >= 150000) {
+  roip += 0.2;
+}
+
 var color;
-if (roip >= 0.55) {
-  color = "#308A24";
-} else if (roip <= 0.25) {
-  color = "#BD4231";
+if (roip <= 0.65) {
+  color = "#CC0033";
+} else if(roip >= 0.75 && roip < 1.00) {
+  color = "#FFBB00";
+} else if (roip >= 1.00 && roip < 2.00) {
+  color = "#009900";
+} else if (roip >= 2.00) {
+  color = "#0075F5";
 } else {
   color = "#000";
 }
-currency.append('<br/><span style="font-size: 12px;">Daily ROI: <span style="color:' + color + ';">' + roip.toFixed(2) + '%</span></span>');
+
+if (divamt >= 150000) {
+  roip -= 0.2;
+}
+
+currency.append('<br/><span style="font-size: 12px;">ROI Right Now: <span style="color:' + color + ';">' + roip.toFixed(2) + '%</span></span>');
