416fdc7dfa469656b36ddcf2e5f3e72941528e54
Eddie Ringle <eddie@eringle.net>
2011-05-07 17:03:23 -0400

    Color code ROI
    Will color red if ROI is 0.25% or below
    Will color green if ROI is 0.55% or above
    
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- eavroi.js
+++ eavroi.js
@@ -9,4 +9,12 @@ try {
 var roi = dividend / price;
 var roip = roi * 100;
 
-currency.append('<br/><span style="font-size: 12px;">Daily ROI: ' + roip.toFixed(2) + '%</span>');
+var color;
+if (roip >= 0.55) {
+  color = "#308A24";
+} else if (roip <= 0.25) {
+  color = "#BD4231";
+} else {
+  color = "#000";
+}
+currency.append('<br/><span style="font-size: 12px;">Daily ROI: <span style="color:' + color + ';">' + roip.toFixed(2) + '%</span></span>');
